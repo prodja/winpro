a4ea00da84b6c8dbbd122648b0b00896ab8637f2
prodja <prodjango@yandex.ru>
2017-02-26 19:10:49 +0300

    a

--- entry_point/settings.py
+++ entry_point/settings.py
@@ -31,7 +31,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'winpro.urls'
+ROOT_URLCONF = 'entry_point.urls'
 
 TEMPLATES = [
     {
@@ -49,7 +49,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'winpro.wsgi.application'
+WSGI_APPLICATION = 'entry_point.wsgi.application'
 
 DATABASES = {
     'default': {
--- entry_point/wsgi.py
+++ entry_point/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "winpro.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entry_point.settings")
 
 application = get_wsgi_application()
